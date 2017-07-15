39a4ea31b1ce7d89d124484e3ccf3c6b97c2f534
Julius Te <mail@juliuste.de>
2017-07-15 18:54:06 +0200

    fix error when id not found

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ const extractImage = (res) => {
 const image = (id) =>
     request(wikidata.getReverseClaims('P757', id+'')) // P757: World heritage site ID
     .then(wikidata.simplifySparqlResults)
+    .then((res) => {
+        if(res.length === 0) throw new Error('site not found')
+        return res
+    })
     .then(wikidata.getEntities)
     .then(request)
     .then(wikidata.parse.wd.entities)
     .then(extractImage)
+    .catch((err) => null)
 
 module.exports = image
